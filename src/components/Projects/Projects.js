7f71b6e3c77191323a7393f9161384222918c2f7
Ratin <ratin1589kumar@gmail.com>
2024-05-23 21:22:34 +0530

    New project section added in projects.js file

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
+import heartDisease from "../../Assets/Projects/heart-disease.png";
 import chatWebsite from "../../Assets/Projects/chat_ with_websites.png";
 import EmotionDetection from "../../Assets/Projects/Emotion_detection_using_voice.png";
 import HotelReview from "../../Assets/Projects/Hotel_reviews_ analysis.png";
@@ -18,11 +19,23 @@ function Projects() {
           Here are a few projects I've worked on recently.
         </p>
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
+
+        <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={heartDisease}
+              isBlog={false}
+              title="Heart Disease Prediction"
+              description="Predicting HEART DISEASE using ML. This Project uses various python-based ML and Data Science libraries in an attempt to build a ML model capable of predicting whether or not someone has HEART DISEASE based on their MEDICAL ATTRIBUTES."
+              ghLink="https://github.com/Ratinkumar/Heart_Disease_Prediction"
+              // demoLink=""
+            />
+          </Col>
+
           <Col md={4} className="project-card">
             <ProjectCard
               imgPath={chatWebsite}
               isBlog={false}
-              title="Chat-with-Websites"
+              title="Chat With Websites"
               description="This project is basically to build a chatbot capable of interacting with websites,extracting information, and communicating in user-friendly manner.It leverages the power of LangChain and integrates it with a Streamlit GUI for an enhanced user experience."
               ghLink="https://github.com/Ratinkumar/Chat-with-Websites"
               // demoLink=""
@@ -33,8 +46,8 @@ function Projects() {
             <ProjectCard
               imgPath={EmotionDetection}
               isBlog={false}
-              title="Emotion-detection-using-voice"
-              description="I have to recognize the emotion of a person by taking his/her voice as the input. For this project the emotion doesn’t depend on the words of speaker it only depends on tone and pitch of what speaker spoke. I am recognizing 7 emotions :- anger, disgust, fear, happiness, neutral, sadness, surprise."
+              title="Emotion Detection Using Voice"
+              description="I have to recognize the emotion of a person by taking his/her voice as the input. For this project the emotion doesn't depend on the words of speaker it only depends on tone and pitch of what speaker spoke. I am recognizing 7 emotions :- anger, disgust, fear, happiness, neutral, sadness, surprise."
               ghLink="https://github.com/Ratinkumar/Emotion-detection-using-voice"
               //demoLink=""
             />
@@ -44,7 +57,7 @@ function Projects() {
             <ProjectCard
               imgPath={HotelReview}
               isBlog={false}
-              title="Hotel-Review-Analysis"
+              title="Hotel Review Analysis"
               description="In this Project Hotel review analysis,covers data preprocessing, feature extraction using Doc2Vec and TF-IDF, training a Random Forest Classifier, and evaluating the model using ROC and Precision-Recall curves. It also includes visualizations such as word cloud for the review data and sentiment distribution for positive and negative reviews."
               ghLink="https://github.com/Ratinkumar/Hotel-Review-Analysis"
               //demoLink=""
